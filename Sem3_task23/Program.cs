754c59faf27e98630c4fc4e37f08c989b7373dd9
Bodagovskiy <abodag100@gmail.com>
2022-09-11 23:05:32 +0300

    task_23

---
+++ Sem3_task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27. 
+//5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+GetDashbord(num1);
+void GetDashbord(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"квадрат числа{count} = {count*count*count}");
+        count++;
+    }
+}
